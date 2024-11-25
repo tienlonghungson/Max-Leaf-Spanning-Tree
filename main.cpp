a7e8f098f216375462b86a7db1f06764e13bb8ae
Md. Tahmidur Rafid <amtrafid@gmail.com>
2024-11-25 16:19:44 -0500

    updated main file structure

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include<bits/stdc++.h>
-#include"AbstractAlgorithm.cpp"
+#include"DegreeProbAlgorithm.cpp"
 
 using namespace std;
 
 int main(){
     Graph g(5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(0, 3);
+    g.addEdge(0, 4);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 3);
 
-    cout << g.size() << "\n";
 
+    AbstractAlgorithm* algorithm = new DegreeProbAlgorithm(g);
+
+    algorithm->execute();
     return 0;
 }
