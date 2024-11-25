8ab43a9143d1b63f6d2bb23b16766e3187f449ce
Md. Tahmidur Rafid <amtrafid@gmail.com>
2024-11-25 16:03:29 -0500

    added abstarct algorithm class

---
+++ AbstractAlgorithm.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include"Graph.cpp"
+
+class AbstractAlgorithm{
+    private:
+        Graph graph;
+    public:
+        AbstractAlgorithm(Graph g){
+            graph = g;
+        }
+
+        virtual void initiate(Graph spanningTree) = 0;
+
+        virtual void execute() = 0;
+};
