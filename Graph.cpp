a9e51f48f36b3e0db4ae36c8f5994bbd474cc18d
Md. Tahmidur Rafid <amtrafid@gmail.com>
2024-11-25 15:52:56 -0500

    added graph class

---
+++ Graph.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Graph{
+    private:
+        int n;
+        vector<vector<int>> list;
+    public:
+        Graph(int N){
+            n = N;
+            list = vector<vector<int>>(n);
+        }
+
+        void addEdge(int u, int v){
+            list[u].push_back(v);
+            list[v].push_back(u);
+        }
+
+        int size(){
+            return n;
+        }
+};
