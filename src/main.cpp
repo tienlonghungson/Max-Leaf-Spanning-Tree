044da00bce6c2e6a13026cf58bdbb1d07e08e195
tienlonghungson <nguyentienlong0203hungson@gmail.com>
2024-12-02 14:58:24 -0500

    add read input in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,23 @@
 #include "AbstractAlgorithm.h"
+#include "Graph.h"
 #include "algorithms/DegreeProbAlgorithm.cpp"
 #include "algorithms/GreedyVertexAlgorithm.cpp"
 #include "algorithms/TwoApprox.cpp"
 #include "SpanningTree.h"
+#include <cstdio>
 #include <vector>
 
 using namespace std;
 
 int main(){
-    Graph g(5);
-    g.addEdge(0, 1);
-    g.addEdge(0, 2);
-    g.addEdge(0, 3);
-    g.addEdge(0, 4);
-    g.addEdge(1, 2);
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-    g.addEdge(2, 3);
+    int n, m, a, b;
+
+    scanf("%d %d",&n, &m);
+    Graph g(n);
+    for (int i=0;i<m;++i){
+        scanf("%d %d", &a, &b);
+        g.addEdge(a,b);
+    }
 
 
     AbstractAlgorithm* algorithm = new GreedyVertexAlgorithm(g);
