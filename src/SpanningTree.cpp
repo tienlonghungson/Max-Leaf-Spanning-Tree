c17cf3759b4f3fe704aa11b04e819300b6413047
Md. Tahmidur Rafid <amtrafid@gmail.com>
2024-12-02 19:28:45 -0500

    added the tree verification algorithm

--- src/SpanningTree.cpp
+++ src/SpanningTree.cpp
@@ -1,5 +1,7 @@
 #include "SpanningTree.h"
 #include "Graph.h"
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -10,5 +12,49 @@ SpanningTree::SpanningTree(const Graph& g): Graph(g){}
 SpanningTree::SpanningTree(): Graph(){}
 
 bool SpanningTree::verify(Graph g){
-    return true;
+    int degrees = 0;
+    auto list = getAdjacentList();
+    for(int i = 0; i < size(); i++){
+        degrees += list[i].size();
+    }
+    if (degrees != 2*size() - 2){
+        return false;
+    }
+
+    queue<int> q;
+    q.push(0);
+    vector<bool> visited(size());
+    visited[0] = true;
+
+    while(!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for(auto v : list[u]){
+            if (!visited[v]) {
+                q.push(v);
+                visited[v] = true;
+            }
+        }
+    }
+
+    for(int i = 0; i < size(); i++){
+        if (!visited[i]){
+            return false;
+        }
+    }
+
+    auto graphList = g.getAdjacentList();
+    for(int i = 0; i < graphList.size(); i++){
+        sort(graphList[i].begin(), graphList[i].end());
+    }
+
+    for(int i = 0; i < size(); i++){
+        for(auto v : list[i]){
+            if(!binary_search(graphList[i].begin(), graphList[i].end(), v)){
+                return false;
+            }
+        }
+    }
+
+    return true; 
 }
